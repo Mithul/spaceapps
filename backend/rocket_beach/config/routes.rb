6980b7679d41e0e4ad928b2782b63d1b08b1feed
Saravana Kumar <savkumar90@gmail.com>
2017-04-29 20:29:28 +0530

    WTF?

--- backend/rocket_beach/config/routes.rb
+++ backend/rocket_beach/config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
   resources :beaches
 
   root 'home#index'
   get '/search/beaches' => 'beaches#search', as: :search_beaches
+  devise_for :users, :controllers => {:registrations => "registrations"}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
